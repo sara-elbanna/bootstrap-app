1e57cce76aa712dc29f8a3500f7163e35181fc3e
saralbanna <smf.elbanna@gmail.com>
2018-04-29 20:04:49 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(function(){
+
+
+	
+// Trigger Carousel Slider
+	$('.carousel').carousel({
+			interval: 10000
+			
+		});
+
+// Navbar Links
+	$('.navbar .nav li').click(function(){
+		$('.navbar .nav li').removeClass('active');
+		$(this).addClass('active');
+
+		if($(this).attr('data-distination')){
+			$('html , body').animate({
+			scrollTop:  $('#'+$(this).data('distination')).offset().top - $('.navbar').height()
+			},1000);
+		}
+	});
+
+	$('.navbar .dropdown-menu li').click(function(){
+		$('.navbar button.navbar-toggle').click()
+	})
+
+/* 
+	Theme Colors
+ */
+
+ 	// Gear Icon Click
+ 	$('.theme .colors .icon').click(function(){
+ 		if($(this).hasClass('closed')){
+ 			$(this).parent().parent().css({
+ 			'left':0
+ 			});
+ 			$(this).removeClass('closed')
+ 		}
+ 		else{
+ 			$(this).parent().parent().css({
+ 			'left':-150
+ 			});
+ 			$(this).addClass('closed')
+ 		}
+ 	});
+ 	// Change Theme Colors
+ 	// var scroll_color = $('.theme .colors ul li.active').data('scroll');
+ 	$('.theme .colors ul li').click(function(){
+ 		$('#themes').attr('href',$(this).data('color'));
+ 		$('.theme .colors ul li').removeClass('active');
+ 		$(this).addClass('active');
+ 		scroll_color = $(this).data('scroll');
+ 		$("body").niceScroll("cursorcolor",scroll_color)
+
+ 	});
+
+
+// Nice Scroll
+	
+	$("body").niceScroll({
+	cursorwidth:"16px"
+	});
+	
+
+
+
+});
