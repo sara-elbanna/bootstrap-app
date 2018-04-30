5ae75bc31ec33f4135690e2f5a4800d3de83f035
saralbanna <smf.elbanna@gmail.com>
2018-04-30 19:34:59 +0200

    add scrollbar color

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@ $(function(){
 		}
 	});
 
-	$('.navbar .dropdown-menu li').click(function(){
-		$('.navbar button.navbar-toggle').click()
-	})
+	// $('.navbar .dropdown-menu li').click(function(){
+	// 	$('.navbar button.navbar-toggle').click()
+	// })
 
 /* 
 	Theme Colors
@@ -43,24 +43,70 @@ $(function(){
  			$(this).addClass('closed')
  		}
  	});
+
+
  	// Change Theme Colors
- 	// var scroll_color = $('.theme .colors ul li.active').data('scroll');
+ 	var scroll_color = $('.theme .colors ul li.active').data('scroll');
+ 	var	styles = "<style id='current_css' type='text/css'>body::-webkit-scrollbar { display: block; } body::-webkit-scrollbar-thumb{background-color:"+scroll_color+";}  body::-webkit-scrollbar-track {background: #ededed66;display:block}</style>";  
+
  	$('.theme .colors ul li').click(function(){
  		$('#themes').attr('href',$(this).data('color'));
  		$('.theme .colors ul li').removeClass('active');
  		$(this).addClass('active');
  		scroll_color = $(this).data('scroll');
- 		$("body").niceScroll("cursorcolor",scroll_color)
-
+ 		styles = "<style id='current_css' type='text/css'>body::-webkit-scrollbar { display: block; } body::-webkit-scrollbar-thumb{background-color:"+scroll_color+";}  body::-webkit-scrollbar-track {background: #ededed66;display:block}</style>";  
  	});
 
 
-// Nice Scroll
+	/* Scroll Stop */
+	var scrollStop = function ( callback ) {
+
+		// Make sure a valid callback was provided
+		if ( !callback || Object.prototype.toString.call( callback ) !== '[object Function]' ) return;
+
+		// Setup scrolling variable
+		var isScrolling;
+
+		// Listen for scroll events
+		window.addEventListener('scroll', function ( event ) {
+
+			// Clear our timeout throughout the scroll
+			window.clearTimeout( isScrolling );
+
+			// Set a timeout to run after scrolling ends
+			isScrolling = setTimeout(function() {
+
+				// Run the callback
+				callback();
+
+			}, 66);
+
+		}, false);
+	};
+
+
+ 	var styles_off = "<style id='current_css' type='text/css'>body::-webkit-scrollbar { display: block; }  </style>"
+	$(window).on('scroll',function(){
+		if($('#current_css').length){
+			$('#current_css').replaceWith(styles)
+		}
+		else{
+			$(styles).appendTo('head');
+		}
+	})
+	scrollStop(function(){
+
+		// $('#current_css').remove()
+		$('#current_css').replaceWith(styles_off);
+
+	})
+
+
+
+
+
 	
-	$("body").niceScroll({
-	cursorwidth:"16px"
-	});
-	
+
 
 
 
